3918527464c3c347d4471d2d1c46e00d713da373
Vu Dao <vu.dao@aminocom.com>
2017-09-15 06:57:37 +0300

    Setup for react, babel, webpack re #1

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>This WWIII game that I cannot get out of my head</div>
+        );
+    }
+}
+
+export default App;
