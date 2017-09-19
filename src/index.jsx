21b78325ea5395a74336b77855cb249e2144199a
Vu Dao <anhvudao17@gmail.com>
2017-09-20 01:20:41 +0300

    Add Button to random a big country
    Database from firebase
    re #5

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import firebase from 'firebase/app';
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyC-app2NDbvqGiuKLlSvNT599HcJWatLAY",
+    authDomain: "ww3-game.firebaseapp.com",
+    databaseURL: "https://ww3-game.firebaseio.com",
+    projectId: "ww3-game",
+    storageBucket: "ww3-game.appspot.com",
+    messagingSenderId: "812556191009"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
     <App />,
